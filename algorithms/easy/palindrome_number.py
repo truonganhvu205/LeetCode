26be0f30680762c13c8c9d777dadc5e42ac33296
vu <truonganhvu205@gmail.com>
2024-03-19 09:59:30 +0700

    commit

---
+++ algorithms/easy/palindrome_number.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
+        elif x == int(str(x)[::-1]):
+            return True
+        else:
+            return False
